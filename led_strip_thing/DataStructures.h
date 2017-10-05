e63dd3e5b70f39c0aed94c7324fd345a973f94a8
Jeremiah Lowe <jeremiah_abc123me@hotmail.com>
2017-10-04 20:16:27 -0400

    Add files via upload

---
+++ led_strip_thing/DataStructures.h
@@ -0,0 +1,27 @@
+struct Command{
+    public:
+    String* cmdName;
+    StringArray* params;
+    Command(String name, StringArray pars){
+        cmdName = &name;
+        params = &pars;
+    }
+};
+/*struct StringArray{
+    private:
+    String mainStr = "";
+    int length = 0;
+    public:
+    void AppendString(String s){
+        mainStr += ((char) 0);
+        mainStr += s;
+    }
+    String RemoveLast(){
+        for(int i = mainStr.length(); i >= 0; i--){
+            if(mainStr.charAt(i) == 0){
+                return mainStr.substring(i, mainStr.length());
+            }
+        }
+        return null;
+    }
+}*/
